cf7508e177525831215875473546382cd0a1d496
yurymessias1-code <yurymessias1@gmail.com>
2026-07-03 08:26:30 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.ESTUDOS_TRACK_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://prifxskiaudunlycrygs.supabase.co",
+  anonKey: "sb_publishable_7KkgD_RioKN1l3PFC4ZgdA_qJo0S-kB",
   table: "study_profiles",
 };
